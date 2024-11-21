2dd497bc317b264363cbc84d3a77491ceb0d7adf
lucky-sp4 <00001117591049sp@al.educacao.sp.gov.br>
2024-11-21 08:47:28 -0300

    000000

---
+++ mugango.3.py
@@ -0,0 +1,16 @@
+def print(
+    *values: object,
+    sep: str | None = " ",
+    end: str | None = "\n",
+    file: SupportsWrite[str] | None = None,
+    flush: Literal[False] = False
+) -> None: ...
+
+def print(
+    *values: object,
+    sep: str | None = " ",
+    end: str | None = "\n",
+    file: _SupportsWriteAndFlush[str] | None = None,
+    flush: bool
+    print(fdgw)
+    
